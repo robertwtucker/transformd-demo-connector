a62d133772b7d2dd292d00b7b13f9a06a1c8418c
Robert Tucker <r.tucker@quadient.com>
2023-07-07 14:14:26 -0400

    feat: add Transfomd API client

---
+++ src/api/transformdApiClient.ts
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2021 Quadient Group AG
+ * SPDX-License-Identifier: MIT
+ */
+
+import { TransformdApiAuthTokenResponse } from './transformdApiAuthTokenResponse'
+import { TransformdApiProfileSearchResponse } from './transformdApiSearchProfileResponse'
+
+export class TransformdApiClient {
+  private readonly apiUrl: string
+  private readonly apiKey: string
+  private token: string
+
+  constructor(apiUrl: string, apiKey: string, token = '') {
+    this.apiUrl = apiUrl
+    this.apiKey = apiKey
+    this.token = token
+  }
+
+  /**
+   * Generates a new API token for the user and stores it in the client for
+   * use in subsequent requests. The entire response body is returned as a
+   * convenience.
+   * @returns {Promise<TransformdApiAuthTokenResponse>} Parsed object
+   */
+  async getAuthToken(): Promise<TransformdApiAuthTokenResponse> {
+    const headers = new Headers()
+    headers.append('Accept', 'application/json')
+    headers.append('Authorization', `Bearer ${this.apiKey}`)
+    headers.append('Content-Type', 'application/x-www-form-urlencoded')
+
+    const urlencoded = new URLSearchParams()
+    urlencoded.append('timestamp', Date.now().toString())
+    urlencoded.append('nonce', createGuid())
+
+    const response = await fetch(`${this.apiUrl}/v2/auth/token`, {
+      method: 'POST',
+      headers: headers,
+      body: urlencoded.toString(),
+    })
+
+    if (!response.ok) {
+      throw new Error(`API error: ${response.statusText}-${response.text()}`)
+    }
+
+    return response.json()
+  }
+
+  /**
+   * Search the Profile Records (Dataset Legacy) using the criteria provided
+   * to find the URL for the Form Session initiated by the Webhook.
+   * @param {string} profileId The Profile ID to use
+   * @param {string} searchKey Key value to use in the fields collection
+   * @param {string} searchValue A unique identifier to use as the search value.
+   * @returns {Promise<TransformdApiProfileSearchResponse>} Parsed object
+   */
+  async searchProfile(
+    profileId: string,
+    searchKey: string,
+    searchValue: string
+  ): Promise<TransformdApiProfileSearchResponse> {
+    const headers = new Headers()
+    headers.append('Accept', 'application/json')
+    headers.append('Authorization', `Bearer ${this.apiKey}`)
+    headers.append('Content-Type', 'application/json')
+    headers.append('Token', this.token)
+
+    const body = JSON.parse(`{fields:{${searchKey}:${searchValue}}}`)
+
+    const response = await fetch(
+      `${this.apiUrl}/v2/profile/search?id=${profileId}&scope=1`,
+      {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(body),
+      }
+    )
+
+    if (!response.ok) {
+      throw new Error(`API error: ${response.statusText}-${response.text()}`)
+    }
+
+    return response.json()
+  }
+}
